019a4e7532924d48ce546beab8232d88d60e3912
Asher <asherlamty@gmail.com>
2020-03-28 22:49:55 +0800

    Update

--- profanity-filter/profanity-filter.py
+++ profanity-filter/profanity-filter.py
@@ -10,12 +10,7 @@ from profanity_check import predict
 class ProfanityFilter(commands.Cog):
     """
     A simple filter that checks for profanity in a message and 
-    then deletes it. Many profanity detection libraries use a hard-coded 
-    list of bad words to detect and filter profanity, however this 
-    plugin utilises a library that uses a linear support vector machine 
-    (SVM) model trained on 200k human-labeled samples of clean and profane 
-    text strings. ([`profanity-check`](https://github.com/vzhou842/profanity-check)).
-    Artificial intelligence in a discord bot? Heck yeah!
+    then deletes it.
     """
 
     def __init__(self, bot):
