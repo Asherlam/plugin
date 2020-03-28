0ae817db0270a0c7417f7f4c73215fc4ce715d30
Asher <asherlamty@gmail.com>
2020-03-28 11:26:44 +0800

    Update Dm.py

--- Dm/Dm.py
+++ Dm/Dm.py
@@ -10,7 +10,7 @@ class Dm(commands.Cog):
 
     @commands.command()
     @checks.has_permissions(PermissionLevel.MODERATOR)
-    async def dm(self, ctx, member: discord.Member, *, arg):
+    async def dm(self, ctx, member: discord.Member, *,arg):
         """Dm member in the guild.
         Usage:
         {prefix}dm @member Nice guy
@@ -19,7 +19,7 @@ class Dm(commands.Cog):
             return await ctx.send("Please specify a user")
 
         dmchannel = await member.create_dm()
-        await dmchannel.send(f'{arg}')
+        await dmchannel.send(f"{arg}")
 
 def setup(bot):
 	bot.add_cog(Dm(bot))
