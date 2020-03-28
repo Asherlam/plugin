272ae40eca08f906d84517bd488744d0fd528262
Asher <asherlamty@gmail.com>
2020-03-28 10:13:03 +0800

    Update Reply.py

--- Reply/Reply.py
+++ Reply/Reply.py
@@ -10,7 +10,7 @@ class Rely(commands.Cog):
         if message.author.bot:
             return
 
-        if message.channe.id != 690744388623663144:
+        if message.channel.id != 690744388623663144:
 
             if  message.content.lower() == "check":
                 await message.channel.send("Please use commands in <#{690744388623663144}> Thank you!")
